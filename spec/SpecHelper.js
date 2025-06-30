e87a55063ea4f2229c2f788a23545d5cfb1f4e1e
Manny27nyc <Fordamboy1@gmail.com>
2025-06-30 16:22:07 -0400

    🔒 Inject minimal licensing header (automated)

--- spec/SpecHelper.js
+++ spec/SpecHelper.js
@@ -1,3 +1,4 @@
+// © Licensed Authorship: Manuel J. Nieves (See LICENSE for terms)
 beforeEach(function() {
   this.addMatchers({
     toSubclass: function(func) { return this.actual instanceof func; }
--- src/hydrate-bootstrap.js
+++ src/hydrate-bootstrap.js
@@ -1,2 +1,3 @@
+// © Licensed Authorship: Manuel J. Nieves (See LICENSE for terms)
 require('coffee-script')
 module.exports = require('./hydrate.coffee')
